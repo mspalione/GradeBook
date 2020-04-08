307258a075b1246883d45cfc6d1c553883ebf8e2
Melissa Spalione <melissaspalione86@gmail.com>
2020-04-07 20:25:41 -0400

    allowed user input

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -13,9 +13,42 @@ namespace GradeBook
             Name = name;
         }
 
+        //public void AddLetterGrade(char letter)
+        //{
+        //    switch (letter)
+        //    {
+        //        case 'A':
+        //            AddGrade(90);
+        //            break;
+        //        case 'B':
+        //            AddGrade(80);
+        //            break;
+        //        case 'C':
+        //            AddGrade(70);
+        //            break;
+        //        case 'D':
+        //            AddGrade(60);
+        //            break;
+        //        case 'F':
+        //            AddGrade(50);
+        //            break;
+        //        default:
+        //            AddGrade(0);
+        //            break;
+        //    }
+        //}
+
         public void AddGrade(double grade)
         {
-            grades.Add(grade);
+            if(grade <= 100 && grade >= 0)
+            {
+                grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("Grade must be greater than or equal to 0 and less than or equal to 100.");
+                throw new ArgumentException($"Invalid {nameof(grade)}.");
+            }
         }
 
         public Statistics GetStatistics()
@@ -24,22 +57,45 @@ namespace GradeBook
             var highGrade = double.MinValue;            
             var lowGrade = double.MaxValue;
 
-            foreach(double grade in grades) 
+            var index = 0;
+            var count = grades.Count;
+            //foreach(double grade in grades) 
+            while (index < count)
             {
-                if(grade > highGrade) 
+                if (grades[index] > highGrade)
                 {
-                    highGrade = grade;
+                    highGrade = grades[index];
                 }
 
-                if(grade < lowGrade)
+                if (grades[index] < lowGrade)
                 {
-                    lowGrade = grade;
+                    lowGrade = grades[index];
                 }
-            }
+                index++;
+            } 
 
             result.Average = grades.Average();
             result.High = highGrade;
             result.Low = lowGrade;
+
+            switch (result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
 
             return result;
         }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace GradeBook
 {
@@ -11,13 +12,55 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("Scott's gradebook");
-            book.AddGrade(55.1);
-            book.AddGrade(92.1);
-            book.AddGrade(77.5);
+            //loop until q or quit
+            Console.WriteLine("Please enter a grade. Type q to quit.");
+            var input = Console.ReadLine();
+            var lower = input.ToLower();
+            List<double> grades = new List<double>();
+
+            while (true)
+            {
+            newInput:
+                var test = Regex.Replace(input, "[^0-9.]", "");
+                if (input == test)
+                {
+                    var grade = double.Parse(input);
+                    Console.WriteLine($"You entered {input}. \nPlease enter the next grade. Type q to quit.");
+                    book.AddGrade(grade);
+                    grades.Add(grade);
+                }
+                else if (lower == "q" || lower == "quit")
+                {
+                    if(grades.Count == 0)
+                    {
+                        Console.WriteLine("You haven't entered a grade. To quit the program, select 'q' again. Otherwise, enter a grade.");
+                        input = Console.ReadLine();
+                        lower = input.ToLower();
+                        if (lower == "q" || lower == "quit")
+                        {
+                            goto End;
+                        } 
+                        else
+                        {
+                            goto newInput;
+                        }
+                    }
+                    Console.WriteLine("Finished entering new grades. Computing statistics.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("You did not enter a valid grade. Please only enter numbers between 0 and 100.");
+                }
+                input = Console.ReadLine();
+                lower = input.ToLower();
+            }
 
             var stats = book.GetStatistics();
 
-            Console.WriteLine($"Average grade: {stats.Average:N2}  \nHighest grade: {stats.High} \nLowest grade: {stats.Low} ");
+            Console.WriteLine($"Average grade: {stats.Average:N2}  \nHighest grade: {stats.High} \nLowest grade: {stats.Low} \nLetter grade: {stats.Letter}");
+        End:
+            Console.WriteLine("Program complete.");
 
         }
     }
